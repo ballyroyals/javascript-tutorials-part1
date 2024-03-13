5ad6edde9480d9cad518cef14af4bc2ae166dce4
ballyroyals <bilikisuagboola24@gmail.com>
2024-03-13 11:44:44 +0100

    variablesandvalues, data types, variables declaration, basic operator, operator precedence, string and templete literals

---
+++ script.js
@@ -0,0 +1,88 @@
+/* let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("balikis");
+console.log(23);
+
+let firstName = "balikis";
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let mySecondJob = "Teacher";
+
+let PI = 3.1415;
+
+console.log(myFirstJob); 
+
+//data types
+//console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+
+//decleration of variables
+let age = 30;
+age = 31;
+const birthYear = 1991;
+//birthYear = 1990;
+// Area of a triangle
+// 1/2 * base * height
+
+let base = 5;
+let height = 10;
+
+let area = (1 / 2) * base * height;
+console.log(area);
+console.log("My" + " " + "name" + " " + "is" + " " + "Isaac");
+
+console.log("My name is Isaac");
+console.log("My" + "name" + "is" + "Isaac");
+console.log("2" + 2);
+console.log("1" + "1");
+console.log(2 + 2 + 3 + "00");
+//string concartenation
+let firstName = "Isaac";
+let lastName = "Ayorinde";
+let middleName = "Sunday";
+
+console.log(firstName + " " + lastName + " " + middleName);
+
+let x = 15;
+x += 15;
+x -= 10;
+x *= 2;
+x /= 5;
+x /= 2;
+
+x = x ** 2;
+let ageHabeeb = 36;
+let isaacAge = 32;
+
+let eldest = ageHabeeb > isaacAge;
+console.log(eldest);
+
+console.log(ageHabeeb >= isaacAge)
+console.log(ageHabeeb <= isaacAge)
+
+console.log(x);
+console.log(x ** 3);
+
+*/
+
+//strings and template literals
+const firstName = "ayman";
+const job = "web-developer";
+const birthYear = 2023;
+const year = 2024;
+
+const ayman =
+  "i'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+console.log(ayman);
+
+const aymanNew = `i'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(aymanNew);
